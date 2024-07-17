ceff1c734e5f33928e42170999a6700893bb8e10
stefany santos <stefany.chsantos@gmail.com>
2024-07-17 15:32:25 +0100

    9-DELETE/api/comments/comment_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -226,7 +226,7 @@ describe('PATCH /api/articles/:article_id', () => {
   })
 })
 
-describe.only('DELETE /api/comments/:comment_id', () => {
+describe('DELETE /api/comments/:comment_id', () => {
   test('responds 204 status when deleted comment', () => {
     return request(app)
     .delete("/api/comments/3")
